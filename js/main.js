f157159d59179a4c315a04e76ddb1e3b19c614ef
z568702024 <568702024@qq.com>
2018-03-15 16:04:06 +0800

    my project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var wow = new WOW
+	(
+		{
+		boxClass:     'wow',
+		animateClass: 'animated', 
+		offset:       50, 
+		mobile:       true,
+		live:         true,
+		}
+	);
+wow.init();
---
+++ js/smain.js
@@ -0,0 +1,12 @@
+$('.section3 ul li').eq(0).on('click',function(){
+    $('html,body').animate({scrollTop:$('#a').offset().top});
+});
+$('.section3 ul li').eq(1).on('click',function(){
+    $('html,body').animate({scrollTop:$('#b').offset().top});
+});
+$('.section3 ul li').eq(2).on('click',function(){
+    $('html,body').animate({scrollTop:$('#c').offset().top});
+});
+$('.section3 ul li').eq(3).on('click',function(){
+    $('html,body').animate({scrollTop:$('#d').offset().top});
+});
